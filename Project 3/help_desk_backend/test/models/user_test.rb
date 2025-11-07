d0149de8526815527a60f3b93a7d3f7aa68e273f
Amrutha <amrutha.kalle@berkeley.edu>
2025-11-06 19:14:49 -0800

    fixed user model + added user tests

--- Project 3/help_desk_backend/test/models/user_test.rb
+++ Project 3/help_desk_backend/test/models/user_test.rb
@@ -1,24 +1,46 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  def setup
-    @user = User.new(
-      username: 'testuser',
-      password: 'password123'
+
+  # test that duplicate usernames are not saved
+  test "unique username" do
+    user1 = User.new(
+      username: "alice",
+      password: "password",
+      password_confirmation: "password",
+      last_active_at: Time.current
     )
+
+    assert user1.save, "ERROR: user was not saved"
+
+    user2 = User.create(
+      username: "alice",
+      password: "anotherpassword",
+      password_confirmation: "anotherpassword",
+      last_active_at: Time.current
+    )
+
+    assert_not user2.save, "ERROR: user with duplicate username was saved"
   end
 
-  test 'should be valid' do
-    assert @user.valid?
-  end
 
-  test 'unique username' do
-    @user.save
-    duplicate_user = User.new(
-      username: 'testuser',
-      passowrd: 'anotherpassword'
+  # make sure all user attributes are created correctly
+  test "attributes exist" do
+    user = User.new(
+      username: "alice",
+      password: "password",
+      password_confirmation: "password",
+      last_active_at: Time.current
     )
-    assert_not_duplicate.valid?
+
+    assert user.save, "ERROR: user was not saved"
+
+    assert_not_nil user.id,               "ERROR: id should not be null"
+    assert_not_nil user.username,         "ERROR: username should not be null"
+    assert_not_nil user.password_digest,  "ERROR: password should not be null"
+    assert_not_nil user.last_active_at,   "ERROR: last_active_at should not be null"
+    assert_not_nil user.created_at,       "ERROR: created_at should not be null"
+    assert_not_nil user.updated_at,       "ERROR: updated_at should not be null"
   end
 
 end
